86d3f62e5b6d2e48c66207a4cfd8882c96dd4e66
Suspious <59746897+Suspious@users.noreply.github.com>
2021-09-06 12:20:39 +0200

    small change

---
+++ person.py
@@ -0,0 +1,12 @@
+import time
+naam = input("wat is je naam? ")
+adres = input("wat is je adres? ")
+postcode = input("wat is je postcode? ")
+huis = input("waar woon je? ")
+
+print(" ----------------------------------------------------")
+print ("|  Naam     : " + naam)
+print("|  Adres     : " + adres  )
+print("|  Postcode  : " + postcode)
+print("|  Woonplaats: " +  huis)
+print(" ----------------------------------------------------")
